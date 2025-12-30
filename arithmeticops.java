30a81105e625bbbe832b3e64d7876f3672ba1800
Sidharth Mohan <mohansidharth00@gmail.com>
2025-12-30 11:01:21 +0530

    Implementing basic arithmetic operations in Java

---
+++ arithmeticops.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class arithmeticops{
+  public static void main(String[]args){
+    int x = 12;
+    int y = 25;
+    int sum = x+y;
+    int diff = y-x;
+    int prd = x*y;
+    int q = y/x;
+    int r = y%x;
+    System.out.println("Sum of " + x + " and " + y + " is " + sum);
+    System.out.println("Difference between " + x + " and " + y + " is " + diff);
+    System.out.println("Product of " + x + " and " + y + " is " + prd);
+    System.out.println("Quotient when " + y + " is divided by " + x + " is " + q);
+    System.out.println("Remainder when " + y + " is divided by " + x + " is " + r);
+  }
+}
