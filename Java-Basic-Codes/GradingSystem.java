46b8b0b57099f7ae0015d60215bd2fdabc27e985
Sidharth Mohan <mohansidharth00@gmail.com>
2025-12-31 19:38:17 +0530

    Implement grading system based on marks
    Fix variable name from 'x' to 'marks' for grading logic.

---
+++ Java-Basic-Codes/GradingSystem.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class GradingSystem{
+  public static void main(String[]args){
+    int marks = 56;
+    if(x>90){
+      System.out.println("Excellent");
+    }
+    else if(x>80){
+      System.out.println("Good");
+    }
+    else if(x>70){
+      System.out.println("Fair");
+    }
+    else if(x>60){
+      System.out.println("Meets Expectations");
+    }
+    else if(x<60){
+     System.out.println("Below Par"); 
+    }
+  }
+}
