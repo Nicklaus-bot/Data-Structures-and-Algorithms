735fe69eb7e60f84bfc5111e3e9fc733753cfdab
Sidharth Mohan <mohansidharth00@gmail.com>
2025-12-31 11:00:02 +0530

    Check number parity
    Fix variable name in the output statement for odd numbers.

---
+++ Java-Basic-Codes/OddEven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class OddEven{
+  public static void main(String[]args){
+    int x = 15;
+    if(x%2==0){
+      System.out.println(x + " is Even");
+    }
+    else{
+      System.out.println(y + " is Odd");
+    }
+  }
+}
