cc7fa38681e224699ae1debe9ea0a6df49e8e64c
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-01 07:13:42 +0530

    Generating Fibonacci Series till 'n'

---
+++ Java-Basic-Codes/FibonacciNumbers.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+pubic class FibonacciNumbers{
+  public static void main(String[]args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int x = 0;
+    int y = 1;
+    for(int i=0 ; i<n ; i++){
+      System.out.println(x);
+      int z = x+y;
+      x=y;
+      y=z;
+    }
+  }
+}
