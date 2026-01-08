6a6bee13f19c212a87d9e7e5886c46ea8e118f5d
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-08 19:33:32 +0530

    Changes to Triangle4 code
    Made a correction of <=  to < in star loop.

--- Patterns/Triangle4.java
+++ Patterns/Triangle4.java
@@ -9,7 +9,7 @@ public class Triangle4{
             for(int j=0 ; j<sp ; j++){
                 System.out.print("\t");
             }
-            for(int j=0 ; j<=st ; j++){
+            for(int j=0 ; j<st ; j++){
                 System.out.print("*\t");
             }
 
