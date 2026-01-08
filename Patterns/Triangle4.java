f8fe72643a6088af0266cd4803b16aaa8ddd4307
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-08 18:55:36 +0530

    Type 4 of Triangle Pattern.
    For eg -
    n = 5;
        *
       **
      ***
     ****
    *****

---
+++ Patterns/Triangle4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Triangle4{
+    public static void main(String[]args) throws Exception{
+        Scanner scn = new  Scanner(System.in);
+        int n = scn.nextInt();
+        int sp = n-1;
+        int st = 1;
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<sp ; j++){
+                System.out.print("\t");
+            }
+            for(int j=0 ; j<=st ; j++){
+                System.out.print("*\t");
+            }
+
+            System.out.println();
+            sp--;
+            st++;
+        }
+    }
+}
