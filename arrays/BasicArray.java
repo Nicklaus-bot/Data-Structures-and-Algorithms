d5fda215fc33db51b6fcf6b6410046ad6cf4f766
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-09 19:49:21 +0530

    Basic IO code for printing an Array

--- arrays/BasicArray.java
+++ arrays/BasicArray.java
@@ -1,5 +1,16 @@
-import java.io.*;
 import java.util.*;
 public class BasicArray{
-    
+    public static void main(String[]args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] arr = new int [n];
+        for(int i=0 ; i<n ; i++){
+            arr[i] = scn.nextInt();
+        }
+
+        for(int val : arr){
+            System.out.print(val + " ");
+        }
+
+    }
 }
