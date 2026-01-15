796348232625c710d3891c6690644b728fa3490c
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-15 08:19:08 +0530

    Convert a number from base system to decimal system

---
+++ functions/BaseToDecimal.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class BaseToDecimal {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b = scn.nextInt();
+
+        int btd = basetodecimal(n , b);
+        System.out.println("Base to decimal is " + btd);
+    }    
+
+    public static int basetodecimal(int n , int b){
+        int rv = 0;
+        int p = 1;
+        while(n>0){
+            int r = n%10;
+            rv += r*p;
+            p *= b;
+            n = n/10;
+        }
+        return rv;
+    }
+}
