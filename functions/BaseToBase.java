d6091cd4de61b5bbe1146a90832458e1e3c5a9fd
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-16 22:23:22 +0530

    Handled main for the code

--- functions/BaseToBase.java
+++ functions/BaseToBase.java
@@ -1,3 +1,24 @@
-public class BaseToBase {
+import java.io.*;
+import java.util.*;
+public class BaseToBase{
+    public static void main(String [] args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b1 = scn.nextInt();
+        int b2 = scn.nextInt();
+
+        int btd = basetodecimal(n , b1);
+        int dtb = decimaltobase(btd , b2);
+        int btb = dtb;
+        System.out.println(btb);
+    }
+
+    public static int basetodecimal(int n , int b){
+
+    }
+
+    public static int decimaltobase(int n , int b){
+
+    }
     
 }
