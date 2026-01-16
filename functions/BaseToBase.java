3882af91d983a0418a41a087ac68a66c5fd23074
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-16 22:29:05 +0530

    Added the functions code ready

--- functions/BaseToBase.java
+++ functions/BaseToBase.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 public class BaseToBase{
     public static void main(String [] args) throws Exception{
@@ -14,11 +13,29 @@ public class BaseToBase{
     }
 
     public static int basetodecimal(int n , int b){
+        int rv = 0;
+        int p = 1;
 
+        while(n > 0){
+            int r = n%10;
+            n = n/10;
+            rv += r*p;
+            p*=b;
+        }
+
+        return rv;
     }
 
     public static int decimaltobase(int n , int b){
+        int rv = 0;
+        int p = 1;
 
+        while(n>0){
+            int r = n%b;
+            rv += r*p;
+            p *= 10;
+            n = n/b;
+        }
+        return rv;
     }
-    
 }
