930aeb84093c7a501ac3c8e16d9f3bf1b946b108
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-17 20:39:11 +0530

    Main IO for BaseMultiplication Function

---
+++ functions/BaseMultiplication.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class BaseMultiplication{
+    public static void main(String [] args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+        int b = scn.nextInt();
+
+        int prd = baseproduct(n1 , n2 , b);
+
+    }
+
+    public static int singledigitproduct(int n1 , int d , int b){
+        int rv = 0;
+        // code here
+        return rv;
+    }
+
+    public static int baseaddition(int n1 , int n2 , int b){
+        int rv = 0;
+        //code here;
+        return rv;
+    }
+
+    public static int baseproduct(int n1 , int n2 , int b){
+        int rv = 0;
+        // code here;
+        return rv;
+    }
+
+}
