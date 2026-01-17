67b3a3e193249c48601683df21bd619fda12ba65
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-17 20:44:22 +0530

    Added baseaddition function

--- functions/BaseAddition.java
+++ functions/BaseAddition.java
@@ -12,7 +12,23 @@ public class BaseAddition{
 
     public static int baseaddition(int n1 , int n2 , int b){
         int rv = 0;
-        //code here
+        int c = 0;
+        int p = 1;
+        
+        while(n1 != 0  || n2 != 0  || c != 0){
+            int r1 = n1%10;
+            int r2 = n2%10;
+
+            int r = r1 + r2 + c;
+            c = r/b;
+            r = r%b;
+
+            rv += r*p;
+            p*=10;
+
+            n1 = n1/10;
+            n2 = n2/10;
+        }
         return rv;
     }
 }
