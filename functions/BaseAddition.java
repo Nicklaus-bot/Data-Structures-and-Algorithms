39639b6afcf643eb11512f7f04e7b7599966aa46
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-17 20:30:08 +0530

    IO handled for Base Addition function

---
+++ functions/BaseAddition.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BaseAddition{
+    public static void main(String[]args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+        int b = scn.nextInt();
+
+        int sum =  baseaddition(n1 , n2 , b);
+        System.out.println(sum);
+    } 
+
+    public static int baseaddition(int n1 , int n2 , int b){
+        int rv = 0;
+        //code here
+        return rv;
+    }
+}
