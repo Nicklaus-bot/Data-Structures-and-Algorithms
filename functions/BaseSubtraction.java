5077d9627194ecd640148e86a3ba8f2de86c936e
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-17 20:34:52 +0530

    IO main for Base Subtraction Code

---
+++ functions/BaseSubtraction.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BaseSubtraction{
+    public static void main(String[]args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+        int b = scn.nextInt();
+
+        int diff = basesubtraction(n1 , n2 , b);
+        System.out.println(diff);
+    }
+
+    public static int basesubtraction(int n1 , int n2 , int b){
+        int rv = 0;
+        //code here
+        return rv;
+    }
+}
