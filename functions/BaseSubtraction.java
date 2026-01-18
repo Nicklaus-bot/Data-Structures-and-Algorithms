92069fdfc40dd561da2e6f02824a47310cd457a4
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-18 08:55:21 +0530

    Added base subtraction function to the code

--- functions/BaseSubtraction.java
+++ functions/BaseSubtraction.java
@@ -12,7 +12,30 @@ public class BaseSubtraction{
 
     public static int basesubtraction(int n1 , int n2 , int b){
         int rv = 0;
-        //code here
+        int c = 0;
+        int p = 1;
+
+        while(n2>0){
+            int r2 = n2%10;
+            int r1 = n1%10;
+
+            int r = r2 - r1 + c;
+            if(r < 0){
+                r = r+b;
+                c = -1;
+            }
+            else{
+                c = 0;
+            }
+
+            n2 = n2/10;
+            n1 = n1/10;
+
+            rv += r*p;
+            p*=10;
+
+        }
+
         return rv;
     }
 }
