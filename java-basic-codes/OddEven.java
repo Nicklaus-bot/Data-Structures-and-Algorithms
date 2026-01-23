d34550cbc9b91525b102d7fd5f07a2f0105a6abe
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-23 08:17:06 +0530

    made some changes and ran test cases

--- java-basic-codes/OddEven.java
+++ java-basic-codes/OddEven.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class OddEven{
   public static void main(String[]args){
-    int x = 15;
+    Scanner scn = new Scanner(System.in);
+    int x = scn.nextInt();
     if(x%2==0){
       System.out.println(x + " is Even");
     }
