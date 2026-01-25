94e0e779014359320c3d84c3e74807e95448b798
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-25 21:44:11 +0530

    changes

--- stacks-and-queues/BalancedBrackets.java
+++ stacks-and-queues/BalancedBrackets.java
@@ -2,5 +2,7 @@ import java.io.*;
 public class BalancedBrackets {
     public static void main(String[]args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
     }    
 }
