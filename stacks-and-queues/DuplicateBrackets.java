7363b570b4e1ddaa1b7d42a00df695b79aef36aa
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-26 20:19:50 +0530

    completed and tested

--- stacks-and-queues/DuplicateBrackets.java
+++ stacks-and-queues/DuplicateBrackets.java
@@ -1,5 +1,31 @@
+import java.util.*;
 public class DuplicateBrackets {
     public static void main(String[]args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0 ; i<exp.length() ; i++){
+            char c = exp.charAt(i);
+
+            if(c == ')'){
+                if(st.peek() == '('){
+                    System.out.println("True");
+                    return;
+                }
+                else{
+                    while(st.peek() != '('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            }
+            else{
+                st.push(c);
+            }
+        }
+        System.out.println("False");
         
     }    
 }
