47353165318b9b6adfc3c842b41e6c5533b429f2
Sidharth Mohan <mohansidharth00@gmail.com>
2026-01-29 08:40:58 +0530

    next smaller element to the right

--- stacks-and-queues/NSERight.java
+++ stacks-and-queues/NSERight.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class NGELeft{
+public class NSERight{
     public static void display(int [] arr){
         StringBuilder sb = new StringBuilder();
         for(int val : arr){
@@ -8,6 +8,7 @@ public class NGELeft{
         }
         System.out.println(sb);
     }
+
     public static void main(String[]args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -15,27 +16,27 @@ public class NGELeft{
         for(int i=0 ; i<arr.length ; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int [] nge = ngeLeft(arr);
-        display(nge);
+        int [] nse = nseRight(arr);
+        display(nse);
     }
 
-    public static int [] ngeLeft(int [] arr){
-        int [] nge = new int [arr.length];
+    public static int [] nseRight(int [] arr){
+        int [] nse = new int [arr.length];
         Stack<Integer> st = new Stack<>();
-        nge[0] = -1;
-        st.push(0);
-        for(int i=1 ; i<arr.length ; i++){
-            while(st.size() > 0 &&  arr[i]>=arr[st.peek()]){
+        st.push(arr.length - 1);
+        nse[arr.length - 1] = -1;
+        for(int i=arr.length - 2 ; i>=0 ; i--){
+            while(st.size() > 0  &&  arr[st.peek()] >= arr[i]){
                 st.pop();
             }
             if(st.size() == 0){
-                nge[i] = -1;
+                nse[i] = -1;
             }
             else{
-                nge[i] = arr[st.peek()];
+                nse[i] = arr[st.peek()];
             }
             st.push(i);
         }
-        return nge;
+        return nse;
     }
 }
