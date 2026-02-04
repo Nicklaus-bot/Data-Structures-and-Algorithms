62cf3dcde6f3a41b687bad780aa5e1a91c6f4a14
Sidharth Mohan <mohansidharth00@gmail.com>
2026-02-04 12:18:56 +0530

    infix evaluation for single digit numbers

---
+++ stacks-and-queues/InfixEvaluation.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+public class InfixEvaluation{
+    public static void main(String[]args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+
+        Stack<Integer> operand = new Stack<>();
+        Stack<Character> operator = new Stack<>();
+
+        for(int i=0 ; i<exp.length() ; i++){
+            char c = exp.charAt(i);
+
+            if(c == '('){
+                operator.push(c);
+            }
+            else if(c == ')'){
+                while(operator.peek() != '('){
+                    int v2 = operand.pop();
+                    int v1 = operand.pop();
+                    char op = operator.pop();
+                    int val = ops(v1 , v2 , op);
+                    operand.push(val);
+                }
+                operator.pop();
+            }
+            else if(c=='/' || c=='*' || c=='-' || c=='+'){
+                while(operator.size()>0 && precedence(c)<=precedence(operator.peek()) && operator.peek() != '('){
+                    int v2 = operand.pop();
+                    int v1 = operand.pop();
+                    char op = operator.pop();
+                    int val = ops(v1 , v2 , op);
+                    operand.push(val);
+                }
+                operator.push(c);
+            }
+            else if(Character.isDigit(c)){
+                operand.push(c - '0');
+            }
+        }
+
+        while(operator.size() > 0){
+            int v2 = operand.pop();
+            int v1 = operand.pop();
+            char op = operator.pop();
+            int val = ops(v1 , v2 , op);
+            operand.push(val);
+        }
+
+        System.out.println(operand.pop());
+    }
+
+    public static int precedence(char c){
+        if(c=='/' || c=='*'){
+            return 2;
+        }
+        else if(c=='-' || c=='+'){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public static int ops(int v1 , int v2 , char c){
+        int val = 0;
+        if(c=='/'){
+            val = v1/v2;
+        }
+        else if(c=='*'){
+            val = v1*v2;
+        }
+        else if(c=='-'){
+            val = v1-v2;
+        }
+        else if(c=='+'){
+            val = v1+v2;
+        }
+        return val;
+    }
+}
