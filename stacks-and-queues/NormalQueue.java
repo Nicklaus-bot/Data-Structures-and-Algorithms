1d38c2488713fa88975b5484e7d41befc35251a3
Sidharth Mohan <mohansidharth00@gmail.com>
2026-02-11 19:55:00 +0530

    IO handling of normal stack

---
+++ stacks-and-queues/NormalQueue.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+public class NormalQueue{
+    public static class NQ{
+        int [] arr;
+        int size;
+        int front;
+
+        public NQ(int cap){
+            arr = new int [cap];
+            size = 0;
+            front = 0;
+        }
+
+        public void add(int val){
+            //code here
+        }
+
+        public int size(){
+            //code here
+        }
+
+        public int remove(){
+            //code here
+        }
+
+        public int peek(){
+            //code here
+        }
+
+        public void display(){
+            //code here
+        }
+        
+    }
+    public static void main(String[]args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        NQ que = new NQ(n);
+        String str = br.readLine();
+
+        while(str.equals("quit")==false){
+            if(str.startsWith("add")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                que.add(val);
+            }
+            else if(str.startsWith("remove")){
+                int val = que.remove();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            }
+            else if(str.startsWith("size")){
+                System.out.println(que.size());
+            }
+            else if(str.startsWith("peek")){
+                int val = que.peek();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            }
+            else if(str.startsWith("dsplay")){
+                que.display();
+            }
+            str = br.readLine();
+        }
+    }
+}
